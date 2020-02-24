b98940bba534e73cd6e01c900b6d2c3f6db60a14
Glitch (python-os) <none>
2020-02-24 19:59:26 +0000

    🍃🌋 Checkpoint ./start.sh:5152800/363:5280977/170 ./boot.py:5152800/246:5280977/392 ./chat:5152800/4

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
-
-#this is shell ;)
+#this sets it to python3
+#then you can use python in bash files!
+# ?
+#then...?
+# yes! how about we install virtualenv? 
+#i know what it means, not what it does
+#OHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# virtual environments, you know- for simulation or web server frameworks like flask
+# or django.
+#LET US ADD IT TO BOOT
+# pip3 install virtualenv --user
+# installed! its ready and installed locally
