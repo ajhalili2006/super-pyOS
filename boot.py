e1308c97a0545d5d4da48c531f28063425d2d800
Glitch (python-os) <none>
2020-02-26 08:15:50 +0000

    🌎🏮 Checkpoint ./boot.py:5152800/244

--- boot.py
+++ boot.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from time import sleep
 print("\033[3;37;40m **PYOS** \033[0;37;40m \n")
 print("a collab between ProtechCEO and J-tech-foundation \033[0m")
@@ -14,6 +15,12 @@ try:
   os.system("clear")
 except:
   os.system("cls")
+
+print("python version: " + sys.version)
+
+if sys.version < "3.0.0":
+  sys.exit("system quit, python version too low") # shuts down if python version is below 3.0.0
+  
 print("install dependencies?(\033[38;5;28mY\033[0m/\033[38;5;124mN\033[0m)")
 
 installdep = input("> ")
@@ -24,3 +31,4 @@ if installdep == "Y":
   
 if installdep == "N":
   os.system("python3 debug.py")
+  
