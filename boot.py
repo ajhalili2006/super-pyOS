a5064e6f9aa6af5e42863652ef6cc96c925d99fc
Glitch (python-os) <none>
2020-02-27 13:09:33 +0000

    🚤⚾️ Checkpoint ./boot.py:5152800/265

--- boot.py
+++ boot.py
@@ -5,12 +5,6 @@ print("\033[3;37;40m **PYOS** \033[0;37;40m \n")
 print("a collab between ProtechCEO and J-tech-foundation \033[0m")
 
 sleep(3)
-#no, this is pauser so people can read above :)
-#bios is on everything, no matter what ;)
-# ok, but we still gotta install it using boot :)
-# i know, graphical or command-line. well virtualenv is readyz
-#gtg for now, be back in a little bit ;)
-# ive gtg anyway,cya
 try:
   os.system("clear")
 except:
