1c3a969dbe0424dd903d2b850db7824c9935e585
Glitch (python-os) <none>
2020-03-01 18:44:06 +0000

    😋🍟 Checkpoint ./test-directory/index:5152800/82 ./index.html:5152800/103 ./switchos.py:5152800/571

---
+++ switchos.py
@@ -0,0 +1,20 @@
+import os
+import io
+import platform    # modules to use
+import subprocess
+import sys
+import glob
+from time import *
+
+os.system("clear")
+print("\033[1;31mSwitch OS utility\033[1;m")
+print("version 1.0")
+print("")
+print("switch your OS here")
+print("")
+print("")
+print("1. switch execution path")
+
+switcher = input("choose an option: ")
+
+# if switcher == "1":
